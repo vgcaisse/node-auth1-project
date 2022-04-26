383ec9e6fce6625d560e71187f9ecaa48174e920
Visnu <vgcaisse@gmail.com>
2022-04-25 22:00:22 -0400

    getting users and user id

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,8 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
 const router =  require('express').Router()
 const { restricted } = require('../auth/auth-middleware')
+
+const User = require('./users-model')
 /**
   [GET] /api/users
 
@@ -25,7 +27,14 @@ const { restricted } = require('../auth/auth-middleware')
  */
 
   router.get('/', restricted, (req, res, next) => {
-    res.json('users')
+    User.find() 
+      .then(users => {
+        res.json(users)
+      })
+      .catch(err => {
+        next(err)
+      })
+    
   })
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
